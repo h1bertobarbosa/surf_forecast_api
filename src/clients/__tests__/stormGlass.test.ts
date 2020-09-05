986d8da94d50e170aa42d59598c4e55e9fab8b1f
Humberto Barbosa <humberto.obarbosa@gmail.com>
2020-09-05 10:48:34 -0300

    Normalização de dados

--- src/clients/__tests__/stormGlass.test.ts
+++ src/clients/__tests__/stormGlass.test.ts
@@ -10,7 +10,7 @@ describe('StormGlass client', () => {
     const lat = -33.798798;
     const lng = 151.654654;
 
-    axios.get = jest.fn().mockResolvedValue(stormGlassWeather3HoursFixture)
+    axios.get = jest.fn().mockResolvedValue({data: stormGlassWeather3HoursFixture})
 
     const stormGlass = new StormGlass(axios)
     const response = await stormGlass.fetchPoints(lat,lng);
